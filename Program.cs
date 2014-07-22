6073d10659321369b3b2fe4b71c5488b08aaf2d8
Popeye Zhong <zongsoft@gmail.com>
2014-07-22 10:51:53 +0800

    初始化代码库。

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zongsoft.Terminals.Launcher
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Zongsoft.Plugins.Application.Start(Zongsoft.Plugins.Terminals.ApplicationContext.Current, args);
+		}
+	}
+}

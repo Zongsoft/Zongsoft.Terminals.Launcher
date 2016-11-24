e72678a47c478a36607276e517d9ea3a4c4a3cdd
Popeye Zhong <zongsoft@gmail.com>
2016-11-24 17:29:38 +0800

    feat: 去掉原有终端程序集的引用。 :fire_engine: :walking:

--- Program.cs
+++ Program.cs
@@ -7,7 +7,53 @@ namespace Zongsoft.Terminals.Launcher
 	{
 		static void Main(string[] args)
 		{
+			Zongsoft.Plugins.Application.Started += Application_Started;
 			Zongsoft.Plugins.Application.Start(Zongsoft.Terminals.Plugins.ApplicationContext.Current, args);
+		}
+
+		static void Application_Started(object sender, Zongsoft.Plugins.ApplicationEventArgs e)
+		{
+			System.Threading.Tasks.Parallel.For(0, 10, i =>
+			{
+				Zongsoft.Diagnostics.Logger.Error(i.ToString());
+			});
+
+			try
+			{
+				Zongsoft.Diagnostics.Logger.Trace("这是一条 Trace 的消息文本。");
+				Zongsoft.Diagnostics.Logger.Debug("这是一条 Debug 的消息文本。", new Dictionary<string, object>()
+				{
+					{ "Key1", "Value 1" },
+					{ "Key2", DateTime.Now },
+					{ "Key3", Zongsoft.Common.RandomGenerator.GenerateInt64() },
+					{ "Key4", Guid.NewGuid() },
+					{ "Key5", Zongsoft.Common.RandomGenerator.Generate(128) },
+				});
+
+				Zongsoft.Diagnostics.Logger.Info("这是一条 Info 的消息文本。", Zongsoft.Common.RandomGenerator.Generate(128));
+				Zongsoft.Diagnostics.Logger.Warn("这是一条 Warn 的消息文本。", Zongsoft.Common.RandomGenerator.Generate(128));
+
+				Test1();
+			}
+			catch(Exception ex)
+			{
+				Zongsoft.Diagnostics.Logger.Error(ex, System.Threading.Thread.CurrentPrincipal);
+			}
+		}
+
+		static void Test1()
+		{
+			Test2();
+		}
+
+		static void Test2()
+		{
+			Test3();
+		}
+
+		static void Test3()
+		{
+			throw new InvalidOperationException("故意抛出的异常！");
 		}
 	}
 }
